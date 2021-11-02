dac974c3a51536f68a0e4698af4855bcf5fcd5d1
Chukwugozie <electegwu@gmail.com>
2021-11-02 03:11:05 +0100

    interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { map } from 'rxjs/operators/map';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operators/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
+import { pipe } from 'rxjs';
+
+
 
 
 
 @Injectable()
 export class ProductService {
  private _albumUrl = '../assets/album.json';
+ constructor(private _http: Http) { }
 
-  constructor(private _http: Http) { }
-
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(map((response: { json: () => number; }) => response.json()));
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(pipe((response) => <Album>response.json()));
   }
-
+  
 }

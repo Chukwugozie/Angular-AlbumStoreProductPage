f11faf159a486897094fbc626fbc0ef2bba2102b
Chukwugozie <electegwu@gmail.com>
2021-11-05 23:57:19 +0100

    tracklisting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ProductService } from './product.service';
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductService
+    ProductService,
+    ProductTracklistingComponent
   ],
   imports: [
     BrowserModule,
